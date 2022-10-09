1ee2b4b747f12402ccb3078a3edf6e5aa885d3d9
laintime <hzhe111@163.com>
2022-10-09 11:51:58 +0800

    downgrade vue3 to vue2

--- app.py
+++ app.py
@@ -8,5 +8,9 @@ def hello_world():  # put application's code here
     return 'Hello World!'
 
 
+@app.route('/search')
+def search_autocomplete():
+    return "11"
+
 if __name__ == '__main__':
     app.run()
